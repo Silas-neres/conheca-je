8e703ff599a9037d6932f8afd6fca06bc88ea668
Silas Neres de Souza <silas.souza@tse.jus.br>
2023-11-09 16:43:19 -0300

    glossario v4

---
+++ src/js/glossario.js
@@ -0,0 +1,27 @@
+
+const links = document.querySelectorAll(".conheca-je__glossario__regioes .link");
+const tabs = document.querySelectorAll(".conheca-je__glossario__conteudo .tabs");
+
+links.forEach(link => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        
+        // Remove a classe ativa de todos os links
+        links.forEach(l => l.classList.remove("link--ativo"));
+        
+        // Adiciona a classe ativa ao link clicado
+        link.classList.add("link--ativo");
+        
+        // Obtém o valor do atributo "tab" do link clicado
+        const tabId = link.getAttribute("tab");
+        
+        // Remove a classe ativa de todas as abas de conteúdo
+        tabs.forEach(tab => tab.classList.remove("tab--ativo"));
+        
+        // Adiciona a classe ativa à aba de conteúdo correspondente
+        const selectedTab = document.querySelector(`.tabs.tab-${tabId}`);
+        if (selectedTab) {
+            selectedTab.classList.add("tab--ativo");
+        }
+    });
+});
