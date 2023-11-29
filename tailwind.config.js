4dda8e7d92f62e96a39f1e4abbfddd7ec0d2df94
Silas Neres de Souza <silas.souza@tse.jus.br>
2023-11-29 17:46:25 -0300

    quiz fisrt version

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./*.{html,js,sass}"],
   theme: {
-    extend: {},
+    extend: {
+      // spacing: {
+      //   '1': '3.25rem',
+      //   '2': '3.75rem',
+      //   '3': '32rem',
+      //   '4': '13rem',
+      // }
+    },
   },
   plugins: [],
 }
