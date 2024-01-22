01c8f8918124d71ee1dbbea7cfa30e0c3e069d7c
Silas Neres de Souza <silas.souza@tse.jus.br>
2024-01-22 17:31:09 -0300

    gerando dist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./*.{html,js,sass}"],
+  content: [
+    "./src/*.{js,sass}",
+    "./*.html"
+  ],
   theme: {
     extend: {
       // spacing: {
