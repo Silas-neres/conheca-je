4b34a6723c0c951679c19a184581a52749cbc50d
Silas Neres <silasneres75@gmail.com>
2024-01-24 17:32:12 -0300

    aplicando responsividade no glossario

--- src/js/tse-explica.js
+++ src/js/tse-explica.js
@@ -1,38 +1,48 @@
 const titulos = [
-    "Mesário: importante ator para a transparência e a segurança nas eleições",
-    "Representante do TSE destaca re19tados de Missão de Observação das eleições regionais da Colômbia",
-    "Saiba as diferenças entre as sessões jurisdicional e administrativa do TSE",
-    // Adicione mais títulos conforme necessário
-  ];
+  "Mesário: importante ator para a transparência e a segurança nas eleições",
+  "Representante do TSE destaca resultados de Missão de Observação das eleições regionais da Colômbia",
+  "Saiba as diferenças entre as sessões jurisdicional e administrativa do TSE",
+  "Mesário: importante ator para a transparência e a segurança nas eleições",
+  "Representante do TSE destaca resultados de Missão de Observação das eleições regionais da Colômbia",
+  "Saiba as diferenças entre as sessões jurisdicional e administrativa do TSE",
+  // Adicione mais títulos conforme necessário
+];
 
-  const subtitulos = [
-    "Os mesários atuam na condução dos trabalhos na seção eleitoral e são peças fundamentais para garantir o sucesso de uma eleição",
-    "Eleitoras e eleitores da Colômbia foram às urnas no último domingo (29) eleger representantes aos cargos de governador,",
-    "As sessões plenárias ordinárias do Tribunal Superior Eleitoral (TSE), realizadas sempre as terças, às 19h,",
-    // Adicione mais subtítulos conforme necessário
-  ];
+const subtitulos = [
+  "Os mesários atuam na condução dos trabalhos na seção eleitoral e são peças fundamentais para garantir o sucesso de uma kakakkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk",
+  "Eleitoras e eleitores da Colômbia foram às urnas no último domingo (29) eleger representantes aos cargos de governador,",
+  "As sessões plenárias ordinárias do Tribunal Superior Eleitoral (TSE), realizadas sempre às terças, às 19h,",
+  "Os mesários atuam na condução dos trabalhos na seção eleitoral e são peças fundamentais para garantir o sucesso de uma ",
+  "Eleitoras e eleitores da Colômbia foram às urnas no último domingo (29) eleger representantes aos cargos de governador,",
+  "As sessões plenárias ordinárias do Tribunal Superior Eleitoral (TSE), realizadas sempre às terças, às 19h,",
+  // Adicione mais subtítulos conforme necessário
+];
 
-  let currentIndex = 0;
+let currentIndex = 0;
 
-  const tituloElement = document.querySelector(".conheca-je__glossario__fonte-titulo");
-  const subtituloElement = document.querySelector(".conheca-je__glossario__fonte-subtitulo");
+const tituloElement = document.querySelector(".conheca-je__glossario__fonte-titulo");
+const subtituloElement = document.querySelector(".conheca-je__glossario__fonte-subtitulo");
 
-  const botaoAnterior = document.getElementById("anterior");
-  const botaoProximo = document.getElementById("proximo");
+const botaoAnterior = document.getElementById("anterior");
+const botaoProximo = document.getElementById("proximo");
 
-  botaoAnterior.addEventListener("click", () => {
-    currentIndex = (currentIndex - 1 + titulos.length) % titulos.length;
-    atualizarConteudoCard();
-  });
-
-  botaoProximo.addEventListener("click", () => {
-    currentIndex = (currentIndex + 1) % titulos.length;
-    atualizarConteudoCard();
-  });
-
-  function atualizarConteudoCard() {
-    tituloElement.textContent = titulos[currentIndex];
-    subtituloElement.textContent = subtitulos[currentIndex];
-  }
-
+botaoAnterior.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + titulos.length) % titulos.length;
   atualizarConteudoCard();
+});
+
+botaoProximo.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % titulos.length;
+  atualizarConteudoCard();
+});
+
+function atualizarConteudoCard() {
+  tituloElement.textContent = titulos[currentIndex];
+  subtituloElement.textContent = subtitulos[currentIndex];
+
+  // Adicionar overflow: hidden; para garantir que a altura seja restrita ao tamanho do conteúdo
+  tituloElement.style.overflow = 'hidden';
+  subtituloElement.style.overflow = 'hidden';
+}
+
+atualizarConteudoCard();
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,7 @@ export default {
     "./*.html"
   ],
   theme: {
-    extend: {
-      // spacing: {
-      //   '1': '3.25rem',
-      //   '2': '3.75rem',
-      //   '3': '32rem',
-      //   '4': '13rem',
-      // }
-    },
+    //
   },
   plugins: [],
 }
